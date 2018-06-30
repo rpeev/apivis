424d7db8a9b3f7e64c1f3a3f12976d5c11163466
Radoslav Peev <rpeev@ymail.com>
2018-06-30 12:26:40 +0300

    Fix import path

--- example/apivis.node.mjs
+++ example/apivis.node.mjs
@@ -1,6 +1,6 @@
 import apivis, {
   apiStr
 } from '../dist/apivis.universal.mjs';
-//} from './node_modules/apivis/dist/apivis.universal.mjs';
+//} from '../node_modules/apivis/dist/apivis.universal.mjs';
 
 console.log(apiStr(process));
