0f77684317de37c6d2a79c0f741e20cdedd0ee66
Radoslav Peev <rpeev@ymail.com>
2018-07-01 07:50:02 +0300

    Simplify import path

--- example/apivis.node.mjs
+++ example/apivis.node.mjs
@@ -1,4 +1,4 @@
 import apivis, {apiStr} from '../dist/apivis.universal';
-//import apivis, {apiStr} from '../node_modules/apivis/dist/apivis.universal';
+//import apivis, {apiStr} from 'apivis/dist/apivis.universal';
 
 console.log(apiStr(process));
