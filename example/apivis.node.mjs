ee4af6161eed07d1f726631d69fe38e2e2442fd7
Radoslav Peev <rpeev@ymail.com>
2019-04-25 22:59:15 +0300

    Use explicit .mjs ext in example

--- example/apivis.node.mjs
+++ example/apivis.node.mjs
@@ -1,4 +1,4 @@
-import apivis, {apiStr} from '../dist/apivis.universal';
-//import apivis, {apiStr} from 'apivis/dist/apivis.universal';
+import apivis, {apiStr} from '../dist/apivis.universal.mjs';
+//import apivis, {apiStr} from 'apivis/dist/apivis.universal.mjs';
 
 console.log(apiStr(process));
